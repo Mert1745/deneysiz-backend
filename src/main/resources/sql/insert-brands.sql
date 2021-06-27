1742cac5880a21325d9370cb633da734a70073ba
mkose <Mert.Kose@partner.bmwgroup.com>
2021-06-27 13:43:02 +0300

    add insert script

---
+++ src/main/resources/sql/insert-brands.sql
@@ -0,0 +1,8 @@
+insert into brand
+--    id, category, ,certificate,  hasVeganProduct, is safe,  name,              ,  offerInChina , parentCompany, parentCompanySafe, shopName, vegan
+values(1, 'Parfüm', 'LB,Peta',     true,            true,     'Cyristal Deodorant',  false,        'Yok',         true              ,'gratis', true);
+
+
+insert into brand
+--    id, category, ,certificate,  hasVeganProduct, is safe,  name,              ,  offerInChina , parentCompany, parentCompanySafe, shopName, vegan
+values(2, 'Parfüm', 'yok',     true,            true,     'Dr. Organic',  false,        'Yok',         true              ,'gratis, watsons', true);
